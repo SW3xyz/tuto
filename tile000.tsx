820c72b14e385fbc9b49138eefb096c9a4496e89
SW3xyz <alain@studioweb3.xyz>
2022-11-14 14:43:40 +0100

    Delete tile000.tsx

--- tile000.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="tile000" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../../Downloads/ezgif-5-aa63ace049-png-32x32-sprite-png/tile000.png" trans="ff00ff" width="32" height="32"/>
-</tileset>
